84cfd7b3e4ad5ddc1c5c2b168b291c493624512a
EleanorShetlandSTEM <eleanor.hutcheon@uhi.ac.uk>
2024-12-13 20:26:19 +0000

    trial commit marzipano
    trial commit

---
+++ app-files/data.js
@@ -0,0 +1,124 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-gs__0153",
+      "name": "GS__0153",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.17594992273275878,
+          "pitch": 0.09827500311525483,
+          "rotation": 0,
+          "target": "2-gs__0156"
+        },
+        {
+          "yaw": -2.7723797268382597,
+          "pitch": 0.19305438422705734,
+          "rotation": 0,
+          "target": "1-gs__0155"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.10404647259672473,
+          "pitch": 0.0037699450637092013,
+          "title": "Teaching lab",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-gs__0155",
+      "name": "GS__0155",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-gs__0156",
+      "name": "GS__0156",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "qtvr",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
